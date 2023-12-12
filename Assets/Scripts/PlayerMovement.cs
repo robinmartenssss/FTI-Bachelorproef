9dadcdbf725e651e1d83281709ad1fe7f8db00e3
robinmartenssss <robin.martens2@student.howest.be>
2023-12-12 21:20:17 +0100

    added walking up surface and stairs kinda buggy but fixing at the end

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,11 +25,19 @@ public class PlayerMovement : MonoBehaviour
 
 
     public Transform orientation;
+    // public Transform StepStairs;
 
     float horizontalInput;
     float verticalInput;
     float VerticalOutOfBound;
     float HorizontalOutOfBounds;
+
+    [Header("Step Check")]
+
+    public GameObject StepRayLower; 
+    public GameObject StepRayUpper; 
+    public float stepHeight = 0.3f;
+    public float stepSmooth = 0.1f;
 
     Vector3 dir;
 
@@ -42,9 +50,13 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
 
         readyToJump = true;
+
+        StepRayUpper.transform.position  = new Vector3(StepRayUpper.transform.position.x, stepHeight ,StepRayUpper.transform.position.x);
+
     }
 
     // Update is called once per frame
+    
     private void Update()
     {
 
@@ -64,6 +76,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+        stepClimber();
     }
 
 
@@ -122,4 +135,63 @@ public class PlayerMovement : MonoBehaviour
             GameBehaviour.Instance.sceneToMoveTo();
         }
     }
+
+    // public void stepClimber()
+    // {
+    //     RaycastHit hitLower;
+    //     if(Physics.Raycast(StepStairs.transform.position, transform.TransformDirection(-Vector3.up), out hitLower, 2f, ~ignoreMe))
+    //         {
+    //             if(grounded && (StepStairs.transform.position.y - hitLower.point.y <= stepHeight))
+    //             {
+    //                 Vector3 targetVector = new Vector3(rb.position.x, hitLower.point.y, rb.position.z);
+    //                 rb.position = Vector3.Lerp(rb.position, targetVector, Time.deltaTime / 0.1f);
+    //                 rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    //             }
+    //             if(!grounded && (StepStairs.transform.position.y - hitLower.point.y <= stepHeight))
+    //             {
+    //                 rb.position = new Vector3(rb.position.x, hitLower.point.y, rb.position.z);
+    //                 rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    //             }
+    //         }
+    // }
+
+    public void stepClimber()
+        {
+            RaycastHit hitLower;
+            if(Physics.Raycast(StepRayLower.transform.position, transform.TransformDirection(Vector3.down), out hitLower, 0.1f))
+            {
+                Debug.Log("nummer1");
+                Debug.DrawRay(StepRayLower.transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.red, 2.0f);
+                RaycastHit hitUpper;
+                if(!Physics.Raycast(StepRayUpper.transform.position, transform.TransformDirection(Vector3.down), out hitUpper, 0.2f))
+                {
+                    Debug.Log("nummer2");
+                    Debug.DrawRay(StepRayLower.transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.green, 2.0f);
+                    rb.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f); 
+                }
+            }
+
+
+
+            // RaycastHit hitLower45;
+            // if(Physics.Raycast(StepRayLower.transform.position, transform.TransformDirection(1.5f, 0.1), out hitLower45, 0.1f))
+            // {
+            //     RaycastHit hitUpper45;
+            //     if(!Physics.Raycast(StepRayUpper.transform.position, transform.TransformDirection(1.5f, 0.1), out hitUpper45, 0.2f))
+            //     {
+            //         rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f); 
+            //     }
+            // }
+
+            // RaycastHit hitLowerMinus45;
+            // if(Physics.Raycast(StepRayLower.transform.position, transform.TransformDirection(-1.5f, 0.1), out hitLowerMinus45, 0.1f))
+            // {
+            //     RaycastHit hitUpperMinus45;
+            //     if(!Physics.Raycast(StepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0.1), out hitUpperMinus45, 0.2f))
+            //     {
+            //         rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f); 
+            //     }
+            // }
+        }
+    
 }
